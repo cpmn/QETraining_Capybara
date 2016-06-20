2bceacc0a222ea653f24eedc5ae5a6ccbdd45994
unknown <Daniel Jauregui@DJAUREGUIG-DH02.jalasoft.local>
2016-06-20 11:54:56 -0400

    Refractor the code

--- Daniel/Task0615/features/navigation.feature
+++ Daniel/Task0615/features/navigation.feature
@@ -8,20 +8,20 @@ I want to open a browser of "roboform" page and fill its form.
 			And I fill following data:
 			|First Name        | Daniel     |
 			|Last Name         | Jauregui   |
-			|Home Phone Number | 707721799  |
+			|Home Phone Number | 123456789  |
 			|Address 1         | Wiracocha  |
 			|Address 2         | Arawi      |
 			|City              | Cochabamba |
 			|State             | NEW YORK   |
 			|Postal Code       | 0000       |
 			|Company Name      | Jalasoft   |
-			|Company Phone     | 4529182    |
-			|Fax Number        | 4629182    |
+			|Company Phone     | 123456789  |
+			|Fax Number        | 123456789  |
 			And I fill folloqing Payment & Shipping Information:
 			| Shipping Method              | Second Day Air              |
 			| Payment Method               | Visa (Preferred)            |
 			| Name on Credit Card or Check | VISA                        |
-			| Credit Card Number           | 22222222222222              |
+			| Credit Card Number           | 12345678901234              |
 			| Expiration Date              | 07/2016                     |
 			| Enter your Email Address     | daniel.jauregui@outlook.com |
 			| Account Type                 | Corporation                 |
--- Daniel/Task0615/features/step_definitions/form_steps.rb
+++ Daniel/Task0615/features/step_definitions/form_steps.rb
@@ -1,26 +1,5 @@
-Given(/^I open the borwser "([^"]*)"$/) do |page|
-	visit(page)
-end
-
-Given(/^I fill following data:$/) do |input|
+def execute_action(input)
 	table = input.rows_hash
-	table.each_pair do |label,value|
-		if @shoppingCartDic[label] == 'input'
-			name = page.find('td',:text => label)
-			index = 1
-			name.all(:xpath,'(.//following-sibling::input[1])').each do |item|
-				name.find(:xpath,"(.//following-sibling::input[1])[#{index}]").set value	
-				index += 1
-			end
-		elsif @shoppingCartDic[label] == 'select'
-			name = page.find('td',:text => label)
-			name.find(:xpath,'(.//following-sibling::select[1])[1]').select(value)
-		end
-	end
-end
-
-Given(/^I fill folloqing Payment & Shipping Information:$/) do |payment|
-	table = payment.rows_hash
 	table.each_pair do |label,value|
 		if @shoppingCartDic[label] == 'input'
 			name = page.find('td',:text => label)
@@ -48,42 +27,46 @@ Given(/^I fill folloqing Payment & Shipping Information:$/) do |payment|
 	end
 end
 
+
+Given(/^I open the borwser "([^"]*)"$/) do |page|
+	visit(page)
+end
+
+Given(/^I fill following data:$/) do |input|
+	execute_action(input)
+end
+
+Given(/^I fill folloqing Payment & Shipping Information:$/) do |input|
+	execute_action(input)
+end
+
 When(/^I will click in "([^"]*)" button$/) do |button|
 	page.find_button('Reset').click()
 end
 
 Then(/^The all field will be in initial state$/) do
-	expect(page.find_field('02frstname',:type => "text").value()).to be_empty
-	expect(page.find_field('04lastname',:type => "text").value()).to be_empty
-	expect(page.find_field('20homephon_ac',:type => "text").value()).to be_empty
-	expect(page.find_field('20homephon_pr',:type => "text").value()).to be_empty
-	expect(page.find_field('20homephon_sf',:type => "text").value()).to be_empty
-	expect(page.find_field('10address1',:type => "text").value()).to be_empty
-	expect(page.find_field('13adr_city',:type => "text").value()).to be_empty
-	expect(page.find(:xpath, '//Select[@name="14adrstate"]').value()).to be_empty
-	expect(page.find_field('16addr_zip',:type => "text").value()).to be_empty
-	expect(page.find_field('16addr_zip',:type => "text").value()).to be_empty
-	expect(page.find_field('10address2',:type => "text").value()).to be_empty
-	expect(page.find_field('05_company',:type => "text").value()).to be_empty
-	expect(page.find_field('21workphon_ac',:type => "text").value()).to be_empty
-	expect(page.find_field('21workphon_pr',:type => "text").value()).to be_empty
-	expect(page.find_field('21workphon_sf',:type => "text").value()).to be_empty
-	expect(page.find_field('21workphon_ex',:type => "text").value()).to be_empty
-	expect(page.find_field('22faxphone_ac',:type => "text").value()).to be_empty
-	expect(page.find_field('22faxphone_pr',:type => "text").value()).to be_empty
-	expect(page.find_field('22faxphone_sf',:type => "text").value()).to be_empty
-	# Verify Payment & Shipping Information
-	expect(page.find(:xpath, '//Select[@name="ship_methd"]').value()).to eq("0")
-	expect(page.find(:xpath, '//Select[@name="40cc__type"]').value()).to eq("0")
-	expect(page.find_field('44cc_uname',:type => "text").value()).to be_empty
-	expect(page.find_field('41ccnumber',:type => "text").value()).to be_empty
-	expect(page.find(:xpath, '//Select[@name="42ccexp_mm"]').value()).to eq("1")
-	expect(page.find(:xpath, '//Select[@name="43ccexp_yy"]').value()).to eq("2011")
-	expect(page.find_field('24emailadr',:type => "text").value()).to be_empty
-	expect(page.find(:xpath, '//Select[@name="shopr_type"]').value()).to be_empty
-	expect(page.find(:xpath, '(//tr/td[*]/input[@name="31password"])[1]').value()).to be_empty
-	expect(page.find(:xpath, '(//tr/td[*]/input[@name="31password"])[2]').value()).to be_empty
-	expect(page.find_field('32passques',:type => "text").value()).to be_empty
-	expect(page.find_field('newsletter',:type => "checkbox")).not_to be_checked
+
+	# Verify Imputs
+	index = 1
+	page.all(:xpath,'(//form[1])[2]//following-sibling::input[@type="text" or @type="password"]').each do |item|
+		expect(page.find(:xpath,"((//form[1])[2]//following-sibling::input[@type='text' or @type='password'])[#{index}]").value()).to be_empty
+		index += 1
+	end
+
+	# Verify Select
+	index = 1
+	page.all(:xpath,'(//form[1])[2]//following-sibling::select').each do |item|
+		node = page.find(:xpath,"((//form[1])[2]//following-sibling::select[1])[#{index}]") #.value()
+		expect(node.value).to eq("").or eq("1").or eq("0").or eq("2011")
+		index += 1
+	end
+
+	# Verify Check
+	index = 1
+	page.all(:xpath,'(//form[1])[2]//following-sibling::input[@type="checkbox"]').each do |item|
+		expect(page.find(:xpath,"((//form[1])[2]//following-sibling::input[@type='checkbox'])[#{index}]")).not_to be_checked
+		index += 1
+	end
+
 end
 
